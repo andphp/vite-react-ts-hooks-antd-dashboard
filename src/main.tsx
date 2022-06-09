45326d646b4648c9af571d7c68be32d88dc44052
daxiong <417170808@qq.com>
2022-06-09 22:23:14 +0800

    add json-server

--- src/main.tsx
+++ src/main.tsx
@@ -13,48 +13,48 @@ import 'nprogress/nprogress.css'
 // import 'default-passive-events'
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            retry: 0,
-            suspense: true,
-            refetchOnMount: false,
-            refetchOnWindowFocus: false,
-            refetchOnReconnect: true,
-            refetchInterval: false
-        }
+  defaultOptions: {
+    queries: {
+      retry: 0,
+      suspense: true,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: true,
+      refetchInterval: false
     }
+  }
 })
 
 const AxiosProvider = ({ children }: React.PropsWithChildren<unknown>) => {
-    const axiosValue = useMemo(() => {
-        return axios
-    }, [])
+  const axiosValue = useMemo(() => {
+    return axios
+  }, [])
 
-    return <AxiosContext.Provider value={axiosValue}>{children}</AxiosContext.Provider>
+  return <AxiosContext.Provider value={axiosValue}>{children}</AxiosContext.Provider>
 }
 
 ReactDOM.render(
-    // <React.StrictMode>
-    <AxiosProvider>
-        <QueryClientProvider client={queryClient}>
-            <RecoilRoot>
-                <ErrorBoundary
-                    fallbackRender={({ error, resetErrorBoundary }) => (
-                        <div>
+  // <React.StrictMode>
+  <AxiosProvider>
+    <QueryClientProvider client={queryClient}>
+      <RecoilRoot>
+        <ErrorBoundary
+          fallbackRender={({ error, resetErrorBoundary }) => (
+            <div>
                             There was an error! <button onClick={() => resetErrorBoundary()}>Try again</button>
-                            <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>
-                        </div>
-                    )}
-                >
-                    <Suspense fallback={<SuspendFallbackLoading/>}>
-                        <App/>
-                        {/* ↓ 可视化开发工具 */}
-                        {/* <ReactQueryDevtools /> */}
-                    </Suspense>
-                </ErrorBoundary>
-            </RecoilRoot>
-        </QueryClientProvider>
-    </AxiosProvider>,
-    // </React.StrictMode>,
-    document.getElementById('root')
+              <pre style={{ whiteSpace: 'normal' }}>{error.message}</pre>
+            </div>
+          )}
+        >
+          <Suspense fallback={<SuspendFallbackLoading/>}>
+            <App/>
+            {/* ↓ 可视化开发工具 */}
+            {/* <ReactQueryDevtools /> */}
+          </Suspense>
+        </ErrorBoundary>
+      </RecoilRoot>
+    </QueryClientProvider>
+  </AxiosProvider>,
+  // </React.StrictMode>,
+  document.getElementById('root')
 )
